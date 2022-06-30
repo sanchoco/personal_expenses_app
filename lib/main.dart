ae6bf529e2ebffb425150fb527ffab38433c5779
sanchoco <jguuun@gmail.com>
2022-06-30 09:19:07 +0900

    styling container

--- lib/main.dart
+++ lib/main.dart
@@ -1,82 +1,72 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_complete_guide/quiz.dart';
-import 'package:flutter_complete_guide/result.dart';
-import './question.dart';
-import './answer.dart';
+import 'package:flutter_complete_guide/transaction.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  State<StatefulWidget> createState() {
-    return _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
   }
 }
 
-class _MyAppState extends State<MyApp> {
-  var _questionIndex = 0;
-  int _totalScore = 0;
-
+class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
+    Transaction(id: 't2', title: 'Weekly Groceries', amount: 16.53, date: DateTime.now()),
+  ];
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        "questionText": 'What\'s your favorite color?',
-        'answers': [
-          {'text': 'Black', 'score': 10},
-          {'text': 'Red', 'score': 5},
-          {'text': 'Green', 'score': 3},
-          {'text': 'White', 'score': 1},
-        ]
-      },
-      {
-        "questionText": 'What\'s your favorite animal?',
-        'answers': [
-          {'text': 'Dog', 'score': 10},
-          {'text': 'Cat', 'score': 5},
-          {'text': 'Elephant', 'score': 3},
-          {'text': 'Lion', 'score': 1}
-        ]
-      },
-      {
-        "questionText": 'What\'s your favorite food?',
-        'answers': [
-          {'text': 'chickin', 'score': 10},
-          {'text': 'hamburger', 'score': 5},
-          {'text': 'colar', 'score': 3},
-          {'text': 'cheese', 'score': 1},
-        ]
-      },
-    ];
-
-    _answerQuestion(int score) {
-      _totalScore += score;
-      setState(() {
-        _questionIndex += 1;
-      });
-    }
-
-    _resetQuiz() {
-      setState(() {
-        _totalScore = 0;
-        _questionIndex = 0;
-      });
-    }
-
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'My First App',
+    return Scaffold(
+      appBar: AppBar(title: Text('Flutter App')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Card(
+            color: Colors.blue,
+            elevation: 5,
+            child: Container(
+              child: Text('CHART!'),
+              width: double.infinity,
+            ),
           ),
-        ),
-        body: _questionIndex < questions.length
-            ? Quiz(
-                answerQuestion: _answerQuestion,
-                questions: questions,
-                questionIndex: _questionIndex,
-              )
-            : Result(_totalScore, _resetQuiz),
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                child: Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                      decoration: BoxDecoration(border: Border.all(color: Colors.purple, width: 2)),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.purple),
+                        tx.amount.toString(),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          tx.title,
+                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                        ),
+                        Text(
+                          tx.date.toString(),
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            }).toList(),
+          ),
+        ],
       ),
     );
   }
